b8970a52d1af9ca7a26cf9985e9c4cd0a565487c
MIKHAIL SVIATSKII <swatkmz2020@gmail.com>
2026-09-03 22:11:08 +0300

    Создал модель Product и зарегистрировал в админке.

--- apps/eshop/models.py
+++ apps/eshop/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    text = models.TextField(verbose_name="Текст")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        db_table = 'eshop_products'
+
+    def __str__(self):
+        return self.title
