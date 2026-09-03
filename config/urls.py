66f3b39439052f39632b8f3a46847c41c0830674
MIKHAIL SVIATSKII <swatkmz2020@gmail.com>
2026-09-03 18:23:49 +0300

    Добавил отображение Главной страницы

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.http import HttpResponse
+
+def home_page_view(request):
+    return HttpResponse("<h1>Главная страница</h1>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page_view),
 ]
