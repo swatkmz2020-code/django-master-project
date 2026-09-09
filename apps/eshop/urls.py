426128f15b84c2585cd705443b1846d52f59e6df
MIKHAIL SVIATSKII <swatkmz2020@gmail.com>
2026-09-09 21:57:45 +0300

    Добавил навигационное меню

--- apps/eshop/urls.py
+++ apps/eshop/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "eshop"
+
 urlpatterns = [
-    path('', views.home_page_view),
-    path('products/', views.product_list_view),
+    path('', views.home_page_view, name="home_page"),
+    path('products/', views.product_list_view, name="product_list"),
 ]
