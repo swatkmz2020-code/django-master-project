fa9a44c38c71b0cdca10ede24717baa43818f2c8
MIKHAIL SVIATSKII <swatkmz2020@gmail.com>
2026-09-09 19:16:38 +0300

    Реализовал функционал получения списка товаров.

--- apps/eshop/urls.py
+++ apps/eshop/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page_view),
+    path('products/', views.product_list_view),
 ]
--- apps/eshop/views.py
+++ apps/eshop/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from eshop.models import Product
+
 
 def home_page_view(request):
     return render(request, template_name='eshop/index.html')
+
+
+def product_list_view(request):
+    products = Product.objects.all()
+    return render(request, template_name='eshop/product_list.html', context={'products': products})
 
 
