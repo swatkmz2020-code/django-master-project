1f21b58dfeb654b74959da42e736e8dca5432e49
MIKHAIL SVIATSKII <swatkmz2020@gmail.com>
2026-09-11 20:36:01 +0300

    Реализовал функционал добавления товара.

--- apps/eshop/urls.py
+++ apps/eshop/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.home_page_view, name="home_page"),
     path('products/', views.product_list_view, name="product_list"),
     path('products/<int:product_id>/', views.product_detail_view, name='product_detail'),
+    path('products/add/', views.product_add_view, name='product_add'),
 ]
