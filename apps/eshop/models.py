50631a398bb0a6ce26ab23df47e97839c62982e8
MIKHAIL SVIATSKII <swatkmz2020@gmail.com>
2026-09-13 08:41:35 +0300

    Django Forms: forms.Form. Изменил форму для добавления товара.

--- apps/eshop/models.py
+++ apps/eshop/models.py
@@ -1,17 +1,13 @@
 from django.db import models
 from decimal import Decimal
 
-
 # Create your models here.
 
 class Product(models.Model):
-    title = models.CharField(max_length=200, verbose_name="Заголовок")
-    text = models.TextField(verbose_name="Текст")
+    title = models.CharField(max_length=200, verbose_name="Название")
+    text = models.TextField(verbose_name="Описание")
     created_at = models.DateTimeField(auto_now_add=True)
-
     price = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal("0.00"), verbose_name="Цена")
-
-
 
     class Meta:
         verbose_name = 'Товар'
