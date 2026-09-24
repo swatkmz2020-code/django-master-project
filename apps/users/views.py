48aafa95c07cf05c5e002038e4dc462ee3cee8b2
MIKHAIL SVIATSKII <swatkmz2020@gmail.com>
2026-09-24 18:19:33 +0300

    feat(users/logout): Реализовать функционал выхода из аккаунта.

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,7 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
 
 
 def register_view(request):
@@ -24,4 +25,10 @@ def login_view(request):
             return redirect("eshop:home_page")
 
     return render(request, 'users/pages/login.html', {'form': form})
+
+
+@require_POST
+def logout_view(request):
+    logout(request)
+    return redirect("eshop:home_page")
 
