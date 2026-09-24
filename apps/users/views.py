cf56af1a8930e964e0fcf6bd26eb9850252e85e9
MIKHAIL SVIATSKII <swatkmz2020@gmail.com>
2026-09-24 18:13:08 +0300

    feat(users/login): Реализовать функционал входа в аккаунт.

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
 
 
@@ -8,6 +9,19 @@ def register_view(request):
     if request.method == "POST":
         if form.is_valid():
             form.save()
-            return redirect('eshop:home_page')
+            return redirect('users:login')
 
     return render(request, 'users/pages/register.html', {'form': form})
+
+
+
+def login_view(request):
+    form = AuthenticationForm(data=request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("eshop:home_page")
+
+    return render(request, 'users/pages/login.html', {'form': form})
+
