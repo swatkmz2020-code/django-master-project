0e71d506e0350a468b7b9aac8c24b5d6595e585a
MIKHAIL SVIATSKII <swatkmz2020@gmail.com>
2026-09-24 18:00:47 +0300

    feat(users/register): Реализовать функционал регистрации.

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def register_view(request):
+    form = UserCreationForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('eshop:home_page')
+
+    return render(request, 'users/pages/register.html', {'form': form})
